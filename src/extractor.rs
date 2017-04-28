fb2a3e4ef47b7f023e1c8fe388e496d28181551f
Denis Kolodin <deniskolodin@gmail.com>
2017-04-28 14:17:11 +0300

    Rename ExtractError to simple Error

--- src/extractor.rs
+++ src/extractor.rs
@@ -4,11 +4,11 @@ use serde_json::{Value, Map};
 use session::{Request, Array, Object};
 
 #[derive(Debug)]
-pub struct ExtractError<'a> {
+pub struct Error<'a> {
     key: &'a str,
 }
 
-impl<'a> error::Error for ExtractError<'a> {
+impl<'a> error::Error for Error<'a> {
     fn description(&self) -> &str {
         "field not found"
     }
@@ -18,15 +18,15 @@ impl<'a> error::Error for ExtractError<'a> {
     }
 }
 
-impl<'a> fmt::Display for ExtractError<'a> {
+impl<'a> fmt::Display for Error<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         write!(f, "Field {} not provided or have wrong format.", self.key)
     }
 }
 
-impl<'a> From<&'a str> for ExtractError<'a> {
+impl<'a> From<&'a str> for Error<'a> {
     fn from(key: &'a str) -> Self {
-        ExtractError {
+        Error {
             key: key,
         }
     }
@@ -35,48 +35,48 @@ impl<'a> From<&'a str> for ExtractError<'a> {
 /// Interface for access to payload of request.
 pub trait Extractor<T> {
     // TODO Change to Result<T, String> to remove extract_field! macro
-    fn extract<'a>(&mut self, key: &'a str) -> Result<T, ExtractError<'a>>;
+    fn extract<'a>(&mut self, key: &'a str) -> Result<T, Error<'a>>;
 }
 
 impl Extractor<Object> for Request {
-    fn extract<'a>(&mut self, key: &'a str) -> Result<Object, ExtractError<'a>> {
+    fn extract<'a>(&mut self, key: &'a str) -> Result<Object, Error<'a>> {
         self.payload.get(key).and_then(Value::as_object).map(Map::to_owned)
-            .ok_or(ExtractError::from(key))
+            .ok_or(Error::from(key))
     }
 }
 
 impl Extractor<Array> for Request {
-    fn extract<'a>(&mut self, key: &'a str) -> Result<Array, ExtractError<'a>> {
+    fn extract<'a>(&mut self, key: &'a str) -> Result<Array, Error<'a>> {
         self.payload.get(key).and_then(Value::as_array).map(Array::to_owned)
-            .ok_or(ExtractError::from(key))
+            .ok_or(Error::from(key))
     }
 }
 
 impl Extractor<String> for Request {
-    fn extract<'a>(&mut self, key: &'a str) -> Result<String, ExtractError<'a>> {
+    fn extract<'a>(&mut self, key: &'a str) -> Result<String, Error<'a>> {
         self.payload.get(key).and_then(Value::as_str).map(str::to_owned)
-            .ok_or(ExtractError::from(key))
+            .ok_or(Error::from(key))
     }
 }
 
 impl Extractor<i64> for Request {
-    fn extract<'a>(&mut self, key: &'a str) -> Result<i64, ExtractError<'a>> {
+    fn extract<'a>(&mut self, key: &'a str) -> Result<i64, Error<'a>> {
         self.payload.get(key).and_then(Value::as_i64)
-            .ok_or(ExtractError::from(key))
+            .ok_or(Error::from(key))
     }
 }
 
 impl Extractor<f64> for Request {
-    fn extract<'a>(&mut self, key: &'a str) -> Result<f64, ExtractError<'a>> {
+    fn extract<'a>(&mut self, key: &'a str) -> Result<f64, Error<'a>> {
         self.payload.get(key).and_then(Value::as_f64)
-            .ok_or(ExtractError::from(key))
+            .ok_or(Error::from(key))
     }
 }
 
 impl Extractor<bool> for Request {
-    fn extract<'a>(&mut self, key: &'a str) -> Result<bool, ExtractError<'a>> {
+    fn extract<'a>(&mut self, key: &'a str) -> Result<bool, Error<'a>> {
         self.payload.get(key).and_then(Value::as_bool)
-            .ok_or(ExtractError::from(key))
+            .ok_or(Error::from(key))
     }
 }
 
