d2e206d7c15ebc109a62f7b0f3d9da4450c3a5c1
Denis Kolodin <deniskolodin@gmail.com>
2017-04-30 15:52:01 +0300

    Add HasRight trait to simplify access control implementation

--- src/permission.rs
+++ src/permission.rs
@@ -8,6 +8,20 @@ error_chain! {
 pub trait Rights {
 }
 
-pub trait Require<T: Rights> {
-    fn require(&self, right: &T) -> Result<()>;
+pub trait Require<R: Rights> {
+    fn require(&self, right: &R) -> Result<()>;
+}
+
+impl<T: HasRight<R>, R: Rights> Require<R> for T {
+    fn require(&self, right: &R) -> Result<()> {
+        if self.has_right(right) {
+            Ok(())
+        } else {
+            Err(ErrorKind::AccessDenied.into())
+        }
+    }
+}
+
+pub trait HasRight<R: Rights> {
+    fn has_right(&self, right: &R) -> bool;
 }
